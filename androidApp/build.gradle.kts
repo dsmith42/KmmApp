cfd1657c3cb13f1dded2a02bd758c70706d6620e
dsmith42 <dan.smith@kinandcarta.com>
2020-11-05 10:42:34 +0000

    Adds required dependencies

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -17,6 +17,11 @@ dependencies {
     implementation("com.google.android.material:material:1.2.0")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+    implementation("androidx.core.core-ktx:1.3.1")
+    implementation("androidx.recyclerview:recyclerview:1.1.0")
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    implementation("anroidx.cardview:cardview:1.0.0")
 }
 android {
     compileSdkVersion(29)
