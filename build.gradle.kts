0ce8176e7b60c21b4859f22c9010a0ebd91bee58
dsmith42 <dan.smith@kinandcarta.com>
2020-11-05 09:58:01 +0000

    Keep gradle as the same version as kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,10 @@ buildscript {
     }
     val kotlinVersion = "1.4.0"
     val sqlDelightVersion: String by project
+
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
         classpath("com.android.tools.build:gradle:4.1.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion")
         classpath("com.squareup.sqldelight:gradle-plugin:$sqlDelightVersion")
     }
